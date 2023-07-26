1f97829b14f188b6ffb73f1a42e137c71c471393
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-07-26 11:25:48 -0300

    Correção de indentação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,48 @@
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-  /* Verificando se a constante existe */
-  if (navToggle) {
-    navToggle.addEventListener('click', () => {
-      navMenu.classList.add('show-menu');
-    });
-  }
+/* Verificando se a constante existe */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-  /* Menu escondido */
-  if (navClose) {
-    navClose.addEventListener('click', () => {
-      navMenu.classList.remove('show-menu');
-    });
-  }
+/* Menu escondido */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
-  /* Removendo o menu mobile */
-  const navLink = document.querySelectorAll(".nav__link");
+/* Removendo o menu mobile */
+const navLink = document.querySelectorAll(".nav__link");
 
-  const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    // Quando clicamos em cada nav__link, removemos a classe show-menu
-    navMenu.classList.remove('show-menu')
-  }
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // Quando clicamos em cada nav__link, removemos a classe show-menu
+  navMenu.classList.remove("show-menu");
+};
 
-  navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-  /* Mudando o fundo do cabeçalho */
-  const bgHeader = () => {
-    const header = document.getElementById('header')
-    // Quando o scroll é maior que 50 viewport height, adicionamos a classe bg-header à tag header
-    this.scrollY >= 50 ? header.classList.add('bg-header')
-                       : header.classList.remove('bg-header')
-  }
+/* Mudando o fundo do cabeçalho */
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  // Quando o scroll é maior que 50 viewport height, adicionamos a classe bg-header à tag header
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
 
-  window.addEventListener('scroll', bgHeader)
+window.addEventListener("scroll", bgHeader);
 
-  /* ANIMAÇÃO GSAP */
-  gsap.from('.home__img-2', 1.2, { opacity: 0, y: 200, delay: .1 })
-  gsap.from('.home__img-3', 1.2, { opacity: 0, y: 200, delay: .5 })
-  gsap.from('.home__data', 1.2, { opacity: 0, y: -60, delay: 1 })
-  gsap.from('.home__bird-1', 1.2, { opacity: 0, x: -80, delay: 1.1 })
-  gsap.from('.home__bird-2', 1.2, { opacity: 0, x: 80, delay: 1.2 })
-  gsap.from('.home__img-1', 1.2, { opacity: 0, y: 200, delay: 1.2 })
-  gsap.from('.home__img-4', 1.2, { opacity: 0, x: 200, delay: 1.3 })
+/* ANIMAÇÃO GSAP */
+gsap.from(".home__img-2", 1.2, { opacity: 0, y: 200, delay: 0.1 });
+gsap.from(".home__img-3", 1.2, { opacity: 0, y: 200, delay: 0.5 });
+gsap.from(".home__data", 1.2, { opacity: 0, y: -60, delay: 1 });
+gsap.from(".home__bird-1", 1.2, { opacity: 0, x: -80, delay: 1.1 });
+gsap.from(".home__bird-2", 1.2, { opacity: 0, x: 80, delay: 1.2 });
+gsap.from(".home__img-1", 1.2, { opacity: 0, y: 200, delay: 1.2 });
+gsap.from(".home__img-4", 1.2, { opacity: 0, x: 200, delay: 1.3 });
